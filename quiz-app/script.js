e954b416bf2b3e0c0f13be152fc456d34d9d6708
Özlem Özdemir <ozllmozdmrr00@gmail.com>
2022-08-19 15:47:37 +0300

    Quiz app renewed

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -39,3 +39,32 @@ const quizData = [
         correct : 'c'
     }
 ]
+
+const questionEL = document.getElementById('question');
+const  a_text = document.getElementById('a_text'); 
+const  b_text = document.getElementById('b_text');
+const  c_text = document.getElementById('c_text');
+const  d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('btn');
+
+
+let currentQuiz = 0;
+
+
+loadQuiz();
+
+function loadQuiz() {
+
+    const currentQuizData = quizData[currentQuiz];
+    questionEL.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+submitBtn.addEventListener("click", () => {
+    currentQuiz++;
+
+    loadQuiz();
+})
