634cc19334b47c4abbc7c233f73cd4c5f095dbeb
Özlem Özdemir <ozllmozdmrr00@gmail.com>
2022-08-19 14:52:52 +0300

    Quiz app renewed

---
+++ quiz-app/script.js
@@ -0,0 +1,41 @@
+const quizData = [
+    {
+        question: 'Daenerys’in hangi ejderhası mavi alevler kusarak duvarı yıkmayı başarmıştı?',
+        a : 'Turncloak',
+        b : 'Viserion',
+        c : 'Rhaegal',
+        d : 'Drogon',
+        correct: 'b'
+    },
+    {
+        question: 'Lord Voldemort’un gücünü kendinde sandığı mürver asa gerçekte en son kime itaat ediyordu?',
+        a : 'Draco Malfoy',
+        b : 'Severus Snape',
+        c : 'Albus Dumbledore',
+        d : 'Harry Potter',
+        correct : 'd'
+    },
+    {
+        question : 'Hobbitlerin Aragorn ile ilk kez tanıştığı yerin adı hangisidir?',
+        a : 'Neşeli Midilli (Joyful Pony)',
+        b : 'Dans Eden Midilli (Dancing Pony)',
+        c : 'Şahlanan Midilli (Prancing Pony)',
+        correct : 'c'
+    },
+    {
+        question : '"Beşinci günün şafağında doğuya bakın." Gandalf Yüzüklerin Efendisi serisinin hangi filminde bu meşhur repliği söylüyordu?',
+        a : 'Yüzük Kardeşliği',
+        b : 'İki Kule',
+        c : 'Kralın Dönüşü',
+        d : 'Hobbit',
+        correct : 'b'
+    },
+    {
+        question : 'Star-Lord, Gamora, Drax ve Groot hangi film serisinde yer alan karakterlerdir?',
+        a :'Transformers', 
+        b :'Fantastik Dörtlü',
+        c :'Galaksinin Koruyucuları', 
+        d :'Yıldızlararası' ,
+        correct : 'c'
+    }
+]
